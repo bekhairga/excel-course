0b7b39d0d17ce514d47953b6041ab7412e0518d0
BekhaIrga <irgashevbehzod@gmail.com>
2021-09-12 19:38:03 +0500

    Basic plugin setup and a way to setup aliases"

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,34 @@
-var path = require('path');
-
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin')
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: 'development',
     entry: './index.js',
     output: {
-        filename: "bundle.js",
+        filename: "bundle.[hash].js",
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            '@': path.resolve(__dirname,'src'),
+            '@/core': path.resolve(__dirname,'src/core')
+        }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPlugin({
+            template: "index.html"
+        }),
+        new CopyPlugin([
+            {from: path.resolve(__dirname,'src/favicon.ico'), to: 'dist'},
+        ]),
+        new MiniCssExtractPlugin({
+            filename: 'bundle.[hash].css'
+        }),
+
+    ]
 }

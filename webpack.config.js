48a776d78f6402f514cdd30ed95152d4e8c458ae
BekhaIrga <irgashevbehzod@gmail.com>
2021-09-12 16:27:45 +0500

    Basic webpack structure

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+var path = require('path');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: './index.js',
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, 'dist')
+    }
+}
